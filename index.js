ab17e014efca24fc84daae8b4af1269b4f1247bd
jesse zenon <jesse.zenon@gmail.com>
2022-01-17 08:00:25 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.innerHTML = '<h1>victory</h1> <p>ETHAN is the champion</p>';
